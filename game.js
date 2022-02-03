55dbc95ec6c0310561c68c041a4fff0d5dd437ee
Fraelite <fraelitecagnin@gmail.com>
2022-02-03 07:44:15 +0100

    added power-ups with collision

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ var config = {
     backgroundColor: 0x000000,
     scene: [Scene1, Scene2],
     pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade:{
+            debug: false
+        }
+    }
 }
 
 var game = new Phaser.Game(config);    
